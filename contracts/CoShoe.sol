5c92c091b58e613a83ac52bce15e66893e75ec22
Zanele Kubheka <zkubheka95@gmail.com>
2020-03-07 11:49:26 +0200

    CoToken and CoShoes added to interact

---
+++ contracts/CoShoe.sol
@@ -0,0 +1,95 @@
+pragma solidity ^0.5.0;
+
+// need contract interface with CoToken
+import "./CoToken.sol";
+
+contract CoShoe {
+    
+    
+    struct Shoe {
+        address payable owner;
+        bool sold;
+        string name;//url to image
+        string image;
+    }
+    // state variable of 0.5 ether and convert to wei
+    //uint public price = 0.5 * 1e18 wei ;
+    //  state variable shoessold which keeps number of shoes already sold
+    // initialise as zero
+    uint public shoesSold = 0;
+
+    // public array holding instance of shoes
+    Shoe[] public shoes;
+
+    //  map containing balances
+    //mapping (address => uint) public balances;
+
+    address payable public minter;
+
+    // constructor to mint 100 coshoe tokens
+    constructor () internal {
+        // set address of deployer to be owner
+        //  instance within shoe array
+        Shoe memory shoeArray;
+        minter  = msg.sender;
+        shoeArray.owner = minter;
+        shoeArray.name = "";
+        shoeArray.image = "";
+        shoeArray.sold = false;  
+
+        // use mint function to mint 100  tokens to mintere
+        
+        
+        // push instances to Shoe array
+       // balances[minter] = 100;
+        shoes.push(shoeArray);
+    
+    }
+
+    
+    // function called buyhsoe
+    function buyShoe(string memory _name, string memory _image, address addr) public {
+    CoToken cotoken = CoToken(addr);
+    // find pair of shoes that have not been sold yet
+    
+    require(shoesSold >0  || shoesSold <=100, "The are no shoes available.");
+
+    // checks value of attahced function is same as price
+    
+    // need to call balanceOf function from mint function to check if address has money
+    require(cotoken.balances(msg.sender) > 0, "No tokens are owned by buyer");
+    cotoken.transferFrom(msg.sender, minter, 1);
+    
+    bool newSold = true;
+    // push to shoes array
+    shoes.push(Shoe(msg.sender, newSold, _name ,_image));
+    
+    // upgrade value of shoes sold
+    shoesSold++;
+    
+
+    }
+    
+    // function to check purchases
+    function checkPurchases() public view returns(bool[] memory) {
+        //set empty boolean array of length of number of shoes sold which will check
+        // will put true in index of array 
+        bool[] memory _soldarray = new bool[](100);
+
+        for (uint i = 0; i <= shoesSold; i++) {
+            Shoe storage shoe = shoes[i];
+            if( shoe.owner == msg.sender ) {
+                _soldarray[i]= true ;
+            }
+            else {
+                _soldarray[i] = false;
+            }
+        }
+        return _soldarray;
+        
+    }
+
+    
+
+
+}
---
+++ contracts/CoToken.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract CoToken is Ownable {
+    
+    // current token supply
+    uint public TotalSupply = 100;
+// mapping of balances
+    mapping (address => uint) public balances;
+
+        constructor() internal { 
+        balances[msg.sender] = TotalSupply;
+
+    }
+    // functiion transfer tokens between accounts
+    function transferFrom(address _from, address _to, uint numberOfTokens) public {
+        // transfer recipent can only recieve money if there are tokens in balance of sender
+        //require(balances[_from] >= _nCo);
+        balances[_from] -= numberOfTokens;
+        balances[_to] += numberOfTokens;
+        TotalSupply -= numberOfTokens;
+    } 
+    
+    // 
+    //function to calculate price of purchase given
+    //bonding curve where f(x) = 0.01x + 0.2
+    // create constructor with name of owner
+
+    function buyPrice (uint _nCo) internal pure returns(uint) {
+        //bonding curve function
+        //price = f(x)
+        // price in ether
+        
+       uint price = (0.01 * 1e18 wei * _nCo) + (0.2 * 1e18 wei);
+        
+        return price ;
+    }
+    // function to calculate price for sale of nco tokens
+    function sellPrice(uint _nCo) internal pure returns(uint) {
+       
+    uint price = (0.01 * 1e18 wei * _nCo) + (0.2 * 1e18 wei);
+
+        return price ;
+    }
+    
+    // implement mint funtion which will create tokens
+    // requires correct current price to be transferred
+    function mint(address _to, uint numberOfTokens) public payable {
+        // use erc20 mint function
+        //number of tokens bought use buy price function and work backwards
+        
+
+        require(msg.value == buyPrice(numberOfTokens), "Not enough tokens were given.");
+        // call TransferFrom function to mint tokens to account
+        transferFrom(owner(), _to, numberOfTokens);
+        
+    }
+
+    
+    //function ehich will sell tokens back to curve 
+    // can only be called by ownership
+    function burn(address _from, uint _nCo) public onlyOwner {  
+        //add tokens to balance of owner
+        require(balances[_from]>= _nCo);
+        transferFrom(_from, msg.sender, _nCo);
+     
+    }
+    
+    // destroy function which  destrucs contrac
+    // requires owner to be called
+    // only called if all Cotokens belong to owner
+    function destroy() onlyOwner external {
+        //uint ownerBalance = balances[msg.sender];
+
+        require(balances[msg.sender] == 100, "All tokens do not belong to owner");
+        selfdestruct(msg.sender);
+    }
+    
+    
+}
